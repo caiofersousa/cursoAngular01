9800b6a2b2b932004e526f4fc3285a808b46c012
Caio Sousa <caiofersousa@hotmail.com>
2021-11-22 09:35:14 -0300

    Initial commit

---
+++ src/app/meu-primeiro/meu-primeiro.module.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'meu-primeiro-component',
+    template: `
+        <p>Meu Primeiro Component Com Angular 2!</p>
+    `
+})
+export class MeuPrimeiroComponent{}
